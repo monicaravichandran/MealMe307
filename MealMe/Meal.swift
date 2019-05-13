137384e442c9c02dfd8caede9e7b8b837dcef615
cashflow18 <cashflowparados@gmail.com>
2019-05-12 20:19:38 -0700

    Started tableview

---
+++ MealMe/Meal.swift
@@ -0,0 +1,19 @@
+//
+//  Meal.swift
+//  MealMe
+//
+//  Created by Aniketh on 5/9/19.
+//  Copyright © 2019 Kalyan Vejalla. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Meal {
+    let name: String
+    let chef: String
+    let avgRating: Float
+    //let image: UIImage
+  //  let description: String
+  //  let ingredients: [String]
+}
